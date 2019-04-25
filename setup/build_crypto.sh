11245e8b28df99766534e9fdb651854a229d3afe
mainfish <nacreature@gmail.com>
2019-04-25 11:47:09 +0300

    moc os bild

--- setup/build_crypto.sh
+++ setup/build_crypto.sh
@@ -14,21 +14,26 @@ tar zxf openssl-$OPENSSL_VERSION.tar.gz
 
 cd openssl-$OPENSSL_VERSION
 
-sh "$SCRIPT_DIR/build_crypto_impl.sh" iphoneos arm64
-sh "$SCRIPT_DIR/build_crypto_impl.sh" iphoneos armv7s
-sh "$SCRIPT_DIR/build_crypto_impl.sh" iphoneos armv7
-sh "$SCRIPT_DIR/build_crypto_impl.sh" iphonesimulator x86_64
-sh "$SCRIPT_DIR/build_crypto_impl.sh" iphonesimulator i386
+#sh "$SCRIPT_DIR/build_crypto_impl.sh" iphoneos arm64
+#sh "$SCRIPT_DIR/build_crypto_impl.sh" iphoneos armv7s
+#sh "$SCRIPT_DIR/build_crypto_impl.sh" iphoneos armv7
+#sh "$SCRIPT_DIR/build_crypto_impl.sh" iphonesimulator x86_64
+sh "$SCRIPT_DIR/build_crypto_impl.sh" macosx x86_64
 
 
 mkdir -p "$SCRIPT_DIR/../Libraries/openssl/lib"
-xcrun lipo -create .build/iphoneos/arm64/libcrypto.a \
-                   .build/iphoneos/armv7s/libcrypto.a \
-                   .build/iphoneos/armv7/libcrypto.a \
-                   .build/iphonesimulator/x86_64/libcrypto.a \
-                   .build/iphonesimulator/i386/libcrypto.a \
+xcrun lipo -create .build/macosx/x86_64/libcrypto.a \
                    -o "$SCRIPT_DIR/../Libraries/openssl/lib/libcrypto.a"
 cp -rf $TDIR/openssl-$OPENSSL_VERSION/include "$SCRIPT_DIR/../Libraries/openssl/"
+
+#mkdir -p "$SCRIPT_DIR/../Libraries/openssl/lib"
+#xcrun lipo -create .build/macosx/x86_64/libcrypto.a \
+#                   .build/iphoneos/arm64/libcrypto.a \
+#                   .build/iphoneos/armv7s/libcrypto.a \
+#                   .build/iphoneos/armv7/libcrypto.a \
+#                   .build/iphonesimulator/x86_64/libcrypto.a \
+#                   -o "$SCRIPT_DIR/../Libraries/openssl/lib/libcrypto.a"
+#cp -rf $TDIR/openssl-$OPENSSL_VERSION/include "$SCRIPT_DIR/../Libraries/openssl/"
 
 cd -
 rm -rf $TDIR
